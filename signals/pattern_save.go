a6af21599174db94ba8765cdbed6d18182157b8f
Amoz3 <amozwilliams1@gmail.com>
2023-03-09 10:44:04 +1000

    pattern saving

---
+++ signals/pattern_save.go
@@ -0,0 +1,47 @@
+package signals
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+
+	"github.com/youpy/go-wav"
+)
+
+func SavePattern(inputfile, outputFile string) {
+	file, err := os.Create(outputFile)
+	if err != nil {
+		panic(err)
+	}
+
+	// the text body we will save to output file
+	fileData := ""
+
+	wavFile, err := os.Open(inputfile)
+	if err != nil {
+		panic(err)
+	}
+	defer wavFile.Close()
+	wavReader := wav.NewReader(wavFile)
+
+	samples := []wav.Sample{}
+	for {
+		sample, err := wavReader.ReadSamples()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		samples = append(samples, sample...)
+	}
+
+	for _, sample := range samples {
+		entry := wavReader.IntValue(sample, 0)
+		fmt.Printf("entry is %d\n", entry)
+		fileData += fmt.Sprintf("%d,\n", entry)
+	}
+
+	ioutil.WriteFile(file.Name(), []byte(fileData), 0777)
+}
